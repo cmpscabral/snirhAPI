166214d78530aba641db3a1eda7b520565258cf7
Francisco Macedo <francisco.macedo@optimile.eu>
2022-03-26 19:08:33 +0000

    remove old flask app

--- app/crawler/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.crawler.stations import Stations
-from app.crawler.parameters import Parameters
-from app.crawler.networks import Networks
-from app.crawler.data import GetData
--- app/serializers.py
+++
@@ -1,8 +0,0 @@
-from marshmallow import Schema, fields
-
-
-class DataQuerySchema(Schema):
-    station = fields.Integer(required=True)
-    parameter = fields.Integer(required=True)
-    tmin = fields.Date(required=True)
-    tmax = fields.String(required=True)
