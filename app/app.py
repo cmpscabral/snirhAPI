120df1eee67a3b39f8947d68cb9dd10385e25646
Francisco Macedo <francisco.macedo@optimile.eu>
2022-04-01 15:58:21 +0100

    remove cache

--- app/app.py
+++ app/app.py
@@ -40,16 +40,6 @@ app.add_middleware(
 )
 
 
-class Cache(BaseModel):
-    client: Optional[Type[requests.Session]]
-    networks: Optional[List[Network]]
-    network_id: Optional[str]
-    stations: Optional[List[Station]]
-
-
-cache = Cache()
-
-
 @app.get("/", include_in_schema=False)
 async def root():
     return RedirectResponse(url="/redoc")
@@ -57,24 +47,18 @@ async def root():
 
 @app.get("/networks/", response_model=List[Network])
 async def networks():
-    if cache.networks:
-        return cache.networks
-    bot = Networks(session=cache.client)
-    cache.client = bot.session
+    bot = Networks()
     networks = bot.get()
-    cache.networks = networks
+    # cache.networks = networks
     return networks
 
 
 @app.get("/networks/{network_id}", response_model=Network)
 async def network(network_id: str):
-    if not cache.networks:
-        bot = Networks(session=cache.client)
-        cache.client = bot.session
-        networks = bot.get()
-        cache.networks = networks
+    bot = Networks()
+    networks = bot.get()
 
-    network = [n for n in cache.networks if n.id == network_id]
+    network = [n for n in networks if n.id == network_id]
     if network:
         return network[0]
     raise HTTPException(status_code=404, detail=f"Network {network_id} not found")
@@ -82,36 +66,16 @@ async def network(network_id: str):
 
 @app.get("/networks/{network_id}/stations/", response_model=List[Station])
 async def stations(network_id: str):
-    if network_id == cache.network_id:
-        if cache.stations:
-            return cache.stations
-        bot = Stations(session=cache.client, new_network=False)
-    else:
-        bot = Stations(session=cache.client, network_id=network_id)
-
-    cache.network_id = network_id
-    cache.client = bot.session
+    bot = Stations(network_id=network_id)
     stations = bot.get()
-    cache.stations = stations
     return stations
 
 
 @app.get("/networks/{network_id}/stations/{station_id}", response_model=Station)
 async def station(network_id: str, station_id: str):
-    if network_id == cache.network_id:
-        if cache.stations:
-            station = [s for s in cache.stations if s.id == station_id]
-            if station:
-                return station[0]
-        bot = Stations(session=cache.client, new_network=False)
-    else:
-        bot = Stations(session=cache.client, network_id=network_id)
-
-    cache.network_id = network_id
-    cache.client = bot.session
+    bot = Stations(network_id=network_id)
     stations = bot.get()
-    cache.stations = stations
-    station = [s for s in cache.stations if s.id == station_id]
+    station = [s for s in stations if s.id == station_id]
     if station:
         return station[0]
     raise HTTPException(
@@ -125,23 +89,20 @@ async def station(network_id: str, station_id: str):
     response_model=List[Parameter],
 )
 async def params(network_id: str, station_id: str):
-    if network_id == cache.network_id:
-        bot = Parameters(session=cache.client, new_network=False)
-    else:
-        bot = Parameters(session=cache.client, network_id=network_id)
+    bot = Parameters(network_id=network_id)
     return bot.get(station_id)
 
 
 @app.get("/data/", response_model=DataEntryList)
 async def data(
-    station: str = "1627758916",
-    parameter: str = "1849",
+    station_id: str = "1627758916",
+    parameter_id: str = "1849",
     tmin: str = "1980-01-01",
     tmax: str = "2020-12-31",
 ) -> DataEntryList:
     return GetData().get_data(
-        station_id=station,
-        parameter_id=parameter,
+        station_id=station_id,
+        parameter_id=parameter_id,
         tmin=parse_datetime(tmin, format="%Y-%m-%d"),
         tmax=parse_datetime(tmax, format="%Y-%m-%d"),
     )
