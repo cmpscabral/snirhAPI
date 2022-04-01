8dfcbb02f88dc7026c4d2e581cccf8de6c1461b8
Francisco Macedo <francisco.macedo@optimile.eu>
2022-04-01 15:44:28 +0100

    add example responses

--- models.py
+++ models.py
@@ -31,6 +31,9 @@ class Network(BaseModel):
     def validate_optional_str_fields(cls, name: str) -> str:
         return name.strip("*").strip(" ")
 
+    class Config:
+        schema_extra = {"example": {"id": "920123705", "name": "Hidrométrica"}}
+
 
 class Station(BaseModel):
     id: str
@@ -71,6 +74,35 @@ class Station(BaseModel):
             return None
         return field
 
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": "1627743378",
+                "codigo": "19B/01H",
+                "nome": "A-DOS-CUNHADOS",
+                "altitude": "14",
+                "latitude": "39.152",
+                "longitude": "-9.302",
+                "coord_x": "98984.104",
+                "coord_y": "243387.146",
+                "bacia": "RIBEIRAS DO OESTE",
+                "distrito": "LISBOA",
+                "concelho": "TORRES VEDRAS",
+                "freguesia": "A DOS CUNHADOS",
+                "entidade_responsavel_automatica": "Autoridade Nacional da Água",
+                "entidade_responsavel_convencional": "-",
+                "tipo_estacao_automatica": "SENSOR DE NÍVEL",
+                "tipo_estacao_convencional": "-",
+                "entrada_funcionamento_convencional": None,
+                "entrada_funcionamento_automatica": "15-02-2002",
+                "encerramento_convencional": None,
+                "encerramento_automatica": None,
+                "telemetria": False,
+                "estado": "SUSPENSA",
+                "indice_qualidade": None,
+            }
+        }
+
 
 class Parameter(BaseModel):
     id: str
@@ -79,6 +111,9 @@ class Parameter(BaseModel):
     @validator("name")
     def validate_optional_str_fields(cls, name: str) -> str:
         return name.strip("*").strip(" ")
+
+    class Config:
+        schema_extra = {"example": {"id": "1849", "name": "Escoamento mensal"}}
 
 
 class DataEntry(BaseModel):
@@ -89,6 +124,20 @@ class DataEntry(BaseModel):
     def validate_value(cls, value: str) -> float:
         return float(value)
 
+    class Config:
+        schema_extra = {
+            "example": {"timestamp": "1980-01-01T00:00:00", "value": 851030}
+        }
+
 
 class DataEntryList(BaseModel):
     __root__: list[DataEntry]
+
+    class Config:
+        schema_extra = {
+            "example": [
+                {"timestamp": "1980-01-01T00:00:00", "value": 851030},
+                {"timestamp": "1980-01-02T00:00:00", "value": 631010},
+                {"timestamp": "1980-01-03T00:00:00", "value": 231010},
+            ]
+        }
